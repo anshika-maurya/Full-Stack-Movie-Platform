9eaa8bc1525a6bfb81dd8336d072aa8eb24fa298
anshika-maurya <anshikamaurya6400@gmail.com>
2026-03-09 15:34:27 +0530

    changes for api acces in backend

--- src/services/tmdbApi.js
+++ src/services/tmdbApi.js
@@ -1,10 +1,13 @@
 const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
 
-const BASE_URL = "https://api.themoviedb.org/3";
-
 export const fetchTrending = async () => {
-  const res = await fetch(`${BASE_URL}/trending/movie/week?api_key=${API_KEY}`);
+
+  const res = await fetch(
+    `${import.meta.env.VITE_API_URL}/api/tmdb/trending`
+  );
+
   const data = await res.json();
+
   return data.results;
 };
 
